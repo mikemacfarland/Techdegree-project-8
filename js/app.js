d273537b985b4dad2d05a2f6b5d62b4f8233cae2
Mike Macfarland <m_macfarland@live.com>
2021-06-09 16:24:24 -0600

    added js to create popup card, styled popup card in css

--- js/app.js
+++ js/app.js
@@ -1,72 +1,74 @@
 const employeeList = document.querySelector('.employeeSection')
+// const card = employeeList.querySelectorAll('.card')
 let employeeAPI = 'https://randomuser.me/api/?results=12'
-let generatedUsers = new XMLHttpRequest()
 let employeeListHTML = []
 let popupListHTML = []
 let employees = []
 
-fetchData(employeeAPI,generateHTML())
+fetchData(employeeAPI)
 
 //-----------------------
 // EVENT LISTENERS
 //-----------------------
 
-// employeeList.addEventListener('click', (e) =>{
-//     if(e.target === '')generatePopup(e)
-// })
+// card.forEach(card => card.addEventListener('click', (e) =>{
+//     if(e.target.id !== 'email'){
+//         console.log('wow')
+//     }
+
+// }))
 
 //-----------------------
 // HELPER FUNCTIONS
 //-----------------------
 
 // fetch api function with generateHTML() function call integrated
-function fetchData(url,func) {
+function fetchData(url) {
     fetch(url)
     .then(response => response.json())
     .then(data => data.results.forEach(result => employees.push(result)))
-    return func
+    .then(generateHTML())
 }
 
 // generate html for employee tabs helper function
 function generateHTML() {
         employees.forEach(employee => {
-
+            console.log(employee)
         // generate array items for employee cards
         employeeListHTML.push(
             `<div class="card flex">
             <img class="thumbnail" src="${employee.picture.large}" alt="${employee.name.first} ${employee.name.last}">
                 <div class="cardInfo flex">
                     <h3>${employee.name.first} ${employee.name.last}</h3>
-                    <a href="mailto:${employee.email}">Send Email</a>
+                    <a id="email" href="mailto:${employee.email}">Send Email</a>
                     <p>${employee.location.city}</p>
                 </div>
             </div>`)
 
         // generate array items for employee popup cards
         popupListHTML.push(
-
-            `<div class="lightbox">
-                <div class="card flex">
-                    <p class="close">x</p>
-                    <img class="popupthumbnail" src="${employee.picture.large}" alt="${employee.name.first} ${employee.name.last}">
-                    <div class="popup flex">
-                        <h3>${employee.name.first} ${employee.name.last}</h3>
-                        <a href="mailto:${employee.email}">Send Email</a>
-                        <p>${employee.location.city}</p>
-                        <p>${employee.phone}</p>
-                        <p>${employee.location.street.name} ${employee.location.street.number} ${employee.location.state} ${employee.location.postcode}</p>
-                        <p>Birthday: ${employee.dob}</p>
-                    </div>
+            `<div class="card flex">
+                <p class="close">x</p>
+                <img class="popupthumbnail" src="${employee.picture.large}" alt="${employee.name.first} ${employee.name.last}">
+                <div class="popupInfo flex">
+                    <h3>${employee.name.first} ${employee.name.last}</h3>
+                    <a href="mailto:${employee.email}">Send Email</a>
+                    <p class="popupLocation">${employee.location.city}</p>
+                    <p class="phone">${employee.phone}</p>
+                    <p class="birthday">${employee.location.street.name} ${employee.location.street.number} ${employee.location.state} ${employee.location.postcode}</p>
+                    <p>Birthday: ${employee.dob}</p>
                 </div>
             </div>`)
     })
-                             //join array items to be used as one single string inserted into HTML
+                            //  join array items to be used as one single string inserted into HTML
     employeeList.innerHTML = employeeListHTML.join("")
 }
 
-function generatePopup(e){
-    
-}
+// function generatePopup(e){
+//     let lightbox = document.querySelector('.lightbox')
+//     if()
+//     lightbox.innerHTML = 
+// }
 
 
 
