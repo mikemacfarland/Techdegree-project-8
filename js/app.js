2b2b29ff1315e3de37901633f040a8b89c7d28ca
Mike Macfarland <m_macfarland@live.com>
2021-06-09 13:20:18 -0600

    updated js to use fetch method

--- js/app.js
+++ js/app.js
@@ -1,30 +1,72 @@
-var generatedUsers = new XMLHttpRequest()
-employeeList = document.querySelector('.gridSection')
+const employeeList = document.querySelector('.employeeSection')
+let employeeAPI = 'https://randomuser.me/api/?results=12'
+let generatedUsers = new XMLHttpRequest()
+let employeeListHTML = []
+let popupListHTML = []
 let employees = []
-employeeListHTML = []
 
+fetchData(employeeAPI,generateHTML())
 
-// retrieve data from random user generator api - https://randomuser.me/documentation
-generatedUsers.onreadystatechange = () => {
-    if(generatedUsers.readyState === 4 && generatedUsers.status === 200){
-        employees = JSON.parse(generatedUsers.responseText).results
-    
-        employees.forEach(employee => {
-            employeeListHTML.push(
-            `<div class="card">
-            <img src="" alt="">
-            <h2>${employee.name.first} ${employee.name.last}</h2>
-            <a href="mailto:${employee.email}">Send Email</a>
-            <p>${employee.location.city}</p>
-            </div>`)
-            
-        })
-        employeeList.innerHTML = employeeListHTML.join("")
-    }
+//-----------------------
+// EVENT LISTENERS
+//-----------------------
+
+// employeeList.addEventListener('click', (e) =>{
+//     if(e.target === '')generatePopup(e)
+// })
+
+//-----------------------
+// HELPER FUNCTIONS
+//-----------------------
+
+// fetch api function with generateHTML() function call integrated
+function fetchData(url,func) {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => data.results.forEach(result => employees.push(result)))
+    return func
 }
-generatedUsers.open('GET', 'https://randomuser.me/api/?results=12')
-generatedUsers.send()
 
+// generate html for employee tabs helper function
+function generateHTML() {
+        employees.forEach(employee => {
+
+        // generate array items for employee cards
+        employeeListHTML.push(
+            `<div class="card flex">
+            <img class="thumbnail" src="${employee.picture.large}" alt="${employee.name.first} ${employee.name.last}">
+                <div class="cardInfo flex">
+                    <h3>${employee.name.first} ${employee.name.last}</h3>
+                    <a href="mailto:${employee.email}">Send Email</a>
+                    <p>${employee.location.city}</p>
+                </div>
+            </div>`)
+
+        // generate array items for employee popup cards
+        popupListHTML.push(
+
+            `<div class="lightbox">
+                <div class="card flex">
+                    <p class="close">x</p>
+                    <img class="popupthumbnail" src="${employee.picture.large}" alt="${employee.name.first} ${employee.name.last}">
+                    <div class="popup flex">
+                        <h3>${employee.name.first} ${employee.name.last}</h3>
+                        <a href="mailto:${employee.email}">Send Email</a>
+                        <p>${employee.location.city}</p>
+                        <p>${employee.phone}</p>
+                        <p>${employee.location.street.name} ${employee.location.street.number} ${employee.location.state} ${employee.location.postcode}</p>
+                        <p>Birthday: ${employee.dob}</p>
+                    </div>
+                </div>
+            </div>`)
+    })
+                             //join array items to be used as one single string inserted into HTML
+    employeeList.innerHTML = employeeListHTML.join("")
+}
+
+function generatePopup(e){
+    
+}
 
 
 
